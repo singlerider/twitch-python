48b0360fa67ef9e5faa005d5d8f341c11fc75386
Shane <haroboy876@gmail.com>
2015-12-30 01:08:28 -0800

    update twitch fix users insert while loop

--- twitch.py
+++ twitch.py
@@ -1,59 +1,57 @@
 import requests
 import json
-channel = ""
-user = ""
-game = ""
 
 
 class Twitch:
 
-    def __init__(self):
+    def __init__(self, channel, user):
         self.channel = channel
         self.user = user
 
-    def users(self, channel=channel, n=0):
+    def users(self):
+        n = 0
         # this endpoint fails often
-        dummy = {
-            "_links": {}, "chatters_count": 0, "chatters": {
-                "staff": [], "admin": [], "global_mods": [],
-                "viewers": [], "moderators": []}}
-        try:
-            url = "https://tmi.twitch.tv/group/user/" + channel \
-                + "/chatters"
-            resp = requests.get(url=url)
-            data = json.loads(resp.content)
-            return data
-        except ValueError:
-            n += 1
-            if n < 3:
-                return users(channel, n=n)
-            else:
+        while n < 3:
+            dummy = {
+                "_links": {}, "chatters_count": 0, "chatters": {
+                    "staff": [], "admin": [], "global_mods": [],
+                    "viewers": [], "moderators": []}}
+            try:
+                url = "https://tmi.twitch.tv/group/user/" + self.channel \
+                    + "/chatters"
+                resp = requests.get(url=url)
+                data = json.loads(resp.content)
+                return data
+            except ValueError:
+                n += 1
+                if n < 3:
+                    continue
+            except:
                 return dummy
-        except:
-            return dummy
+        return dummy
 
-    def follower_status(self, channel=channel, user=user):
-        url = "https://api.twitch.tv/kraken/users/" + user \
-            + "/follows/channels/" + channel
+    def follower_status(self):
+        url = "https://api.twitch.tv/kraken/users/" + self.user \
+            + "/follows/channels/" + self.channel
         resp = requests.get(url=url)
         data = json.loads(resp.content)
         return data
 
-    def stream(self, channel=channel):
+    def stream(self):
         url = "https://api.twitch.tv/kraken/streams/" + \
-            channel
+            self.channel
         resp = requests.get(url=url)
         data = json.loads(resp.content)
         return data
 
-    def followers(self, channel=channel, limit=5):
+    def followers(self, limit=5):
         url = "https://api.twitch.tv/kraken/channels/" + \
-            channel + "/follows?limit=" + str(limit)
+            self.channel + "/follows?limit=" + str(limit)
         resp = requests.get(url=url)
         data = json.loads(resp.content)
         return data
 
-    def game(self, game=game, limit=5):
+    def game(self, game, limit=5):
         game = game.replace(" ", "%20")
         url = "https://api.twitch.tv/kraken/search/streams?q=" + \
             game + "&limit=" + str(limit)
@@ -61,9 +59,9 @@ class Twitch:
         data = json.loads(resp.content)
         return data
 
-    def highlight(self, channel=channel, limit=5):
+    def highlight(self, limit=5):
         url = "https://api.twitch.tv/kraken/channels/" + \
-            channel + "/videos?limit=" + str(limit)
+            self.channel + "/videos?limit=" + str(limit)
         resp = requests.get(url=url)
         data = json.loads(resp.content)
         return data
